c0fc38e63d0a637d80d681b5d104fe1d51f13faa
Leon Mergen <leon@solatis.com>
2012-10-09 11:40:03 +0200

    Extended test3 to now use 3 servers instead of 1

--- test/test3.cpp
+++ test/test3.cpp
@@ -10,19 +10,39 @@
 
 int main ()
 {
-   paxos::server server ("127.0.0.1", 1337,
-                         [](std::string const & workload) -> std::string
-                         {
-                            return workload;
-                         });
-   server.add ("127.0.0.1", 1337);
-   server.start ();
+   paxos::server::callback_type callback = 
+      [](std::string const & workload) -> std::string
+      {
+         return workload;
+      };
+
+   paxos::server server1 ("127.0.0.1", 1337, callback);
+   paxos::server server2 ("127.0.0.1", 1338, callback);
+   paxos::server server3 ("127.0.0.1", 1339, callback);
+
+   server1.add ("127.0.0.1", 1337);
+   server1.add ("127.0.0.1", 1338);
+   server1.add ("127.0.0.1", 1339);
+
+   server2.add ("127.0.0.1", 1337);
+   server2.add ("127.0.0.1", 1338);
+   server2.add ("127.0.0.1", 1339);
+
+   server3.add ("127.0.0.1", 1337);
+   server3.add ("127.0.0.1", 1338);
+   server3.add ("127.0.0.1", 1339);
+
+   server1.start ();
+   server2.start ();
+   server3.start ();
 
 
 
 
    paxos::client client;
    client.add ("127.0.0.1", 1337);
+   client.add ("127.0.0.1", 1338);
+   client.add ("127.0.0.1", 1339);
    client.start ();
 
    std::map <size_t, std::future <std::string> >     results;
